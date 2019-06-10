ea7f4177540ff4b77c3c54242a59c4a58de89b44
jjklee <jjlee720@gmail.com>
2019-06-10 12:22:44 -0700

    Completed Creating and rendering nested React components

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-// TODO
+// // TODO
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList />
+  </div>
+);
+
+const GroceryList = () => (
+  <ol>
+    <li>Cucumber</li>
+    <li>Kale</li>
+  </ol>
+);
+
+ReactDOM.render(<App />, document.getElementById("app"));
